aa31a8e0d3e8b4d565f9623b39d65a862e94b479
StevenR123 <stevenreninga2@gmail.com>
2024-12-06 11:36:53 -0500

    Layout Update

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,7 +17,7 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta(),
+    /*Component.ContentMeta(),*/
     Component.TagList(),
   ],
   left: [
